9f5919e64ff14a9d34be29601280e681f7a3bb83
Denny <denny.sihol@gmail.com>
2024-04-24 10:59:59 +0700

    delete table user

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,10 +1,8 @@
 var express = require('express');
 const TransactionController = require('../controller/transactionController');
 const router = express.Router();
-const { authenticateToken } = require('../middlewares/authentication');
 
-/* GET users listing. */
-// router.get('/va/bills',authenticateToken , TransactionController.showTransaction);
+
 router.post('/va/bills', TransactionController.showTransaction);
 
 module.exports = router;
