809f5c26641e96583c582e7e2863c7dc1add9cd8
Denny <denny.sihol@gmail.com>
2024-04-24 11:32:01 +0700

    change token endpoint to oauth

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const paymentRouter = require('./payments')
 const { generateToken, authenticateToken } = require('../middlewares/authentication');
 
 
-router.use('/api/v1/token', generateToken)
+router.use('/api/oauth/token', generateToken)
 router.use(authenticateToken, transactionRouter)
 router.use(authenticateToken, paymentRouter)
 
