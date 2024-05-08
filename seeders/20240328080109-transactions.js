f325313197cd12b3f8fc887a977655b35f5b4878
Denny <denny.sihol@gmail.com>
2024-05-08 14:41:29 +0700

    seed new dummy data

--- seeders/20240328080109-transactions.js
+++ seeders/20240328080109-transactions.js
@@ -2,8 +2,8 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('bca_transactions', [{
-      CompanyCode: "12345",
-      CustomerNumber: "ABC0012300DEF",
+      CompanyCode: "14348",
+      CustomerNumber: "087875568390",
       RequestID: "201507131507262221400000001975",
       InquiryStatus: "00",
       InquiryReason: JSON.stringify({ Indonesian: 'Sukses', English: 'Success' }),
@@ -56,8 +56,8 @@ module.exports = {
       updatedAt: new Date()
     },
     {
-      CompanyCode: "12346",
-      CustomerNumber: "ABC0012300DEE",
+      CompanyCode: "14348",
+      CustomerNumber: "081210704479",
       RequestID: "201507131507262221400000001974",
       InquiryStatus: "00",
       InquiryReason: JSON.stringify({ Indonesian: 'Sukses', English: 'Success' }),
@@ -108,7 +108,167 @@ module.exports = {
       PaymentFlagReason: JSON.stringify({}),
       createdAt: new Date(),
       updatedAt: new Date()
-    }], {});
+    },
+    {
+      CompanyCode: "14348",
+      CustomerNumber: "081284618005",
+      RequestID: "201507131507262221400000001975",
+      InquiryStatus: "00",
+      InquiryReason: JSON.stringify({ Indonesian: 'Sukses', English: 'Success' }),
+      CustomerName: "Sihol",
+      CurrencyCode: "IDR",
+      TotalAmount: "150000.00",
+      SubCompany: "00001",
+      DetailBills: JSON.stringify([
+        {
+          BillDescription: {
+            Indonesian: "Maintenance",
+            English: "Maintenance"
+          },
+          BillAmount: "70000.00",
+          BillNumber: "1234567890",
+          BillSubCompany: "00001"
+        },
+        {
+          BillDescription: {
+            Indonesian: "Listrik",
+            English: "Electric"
+          },
+          BillAmount: "50000.00",
+          BillNumber: "1133557799",
+          BillSubCompany: "00001"
+        },
+        {
+          BillDescription: {
+            Indonesian: "Air",
+            English: "Water"
+          },
+          BillAmount: "30000.00",
+          BillNumber: "2244668800",
+          BillSubCompany: "00001"
+        }
+      ]),
+      FreeTexts: JSON.stringify([
+        {
+          Indonesian: "Free Text 1",
+          English: "Free Text 1"
+        },
+        {
+          Indonesian: "Free Text 2",
+          English: "Free Text 2"
+        },
+      ]),
+      AdditionalData: JSON.stringify({}),
+      PaymentFlagReason: JSON.stringify({}),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      CompanyCode: "14348",
+      CustomerNumber: "082387403355",
+      RequestID: "201507131507262221400000001975",
+      InquiryStatus: "00",
+      InquiryReason: JSON.stringify({ Indonesian: 'Sukses', English: 'Success' }),
+      CustomerName: "Sihol",
+      CurrencyCode: "IDR",
+      TotalAmount: "150000.00",
+      SubCompany: "00001",
+      DetailBills: JSON.stringify([
+        {
+          BillDescription: {
+            Indonesian: "Maintenance",
+            English: "Maintenance"
+          },
+          BillAmount: "70000.00",
+          BillNumber: "1234567890",
+          BillSubCompany: "00001"
+        },
+        {
+          BillDescription: {
+            Indonesian: "Listrik",
+            English: "Electric"
+          },
+          BillAmount: "50000.00",
+          BillNumber: "1133557799",
+          BillSubCompany: "00001"
+        },
+        {
+          BillDescription: {
+            Indonesian: "Air",
+            English: "Water"
+          },
+          BillAmount: "30000.00",
+          BillNumber: "2244668800",
+          BillSubCompany: "00001"
+        }
+      ]),
+      FreeTexts: JSON.stringify([
+        {
+          Indonesian: "Free Text 1",
+          English: "Free Text 1"
+        },
+        {
+          Indonesian: "Free Text 2",
+          English: "Free Text 2"
+        },
+      ]),
+      AdditionalData: JSON.stringify({}),
+      PaymentFlagReason: JSON.stringify({}),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      CompanyCode: "14348",
+      CustomerNumber: "089526632439",
+      RequestID: "201507131507262221400000001975",
+      InquiryStatus: "00",
+      InquiryReason: JSON.stringify({ Indonesian: 'Sukses', English: 'Success' }),
+      CustomerName: "Sihol",
+      CurrencyCode: "IDR",
+      TotalAmount: "150000.00",
+      SubCompany: "00001",
+      DetailBills: JSON.stringify([
+        {
+          BillDescription: {
+            Indonesian: "Maintenance",
+            English: "Maintenance"
+          },
+          BillAmount: "70000.00",
+          BillNumber: "1234567890",
+          BillSubCompany: "00001"
+        },
+        {
+          BillDescription: {
+            Indonesian: "Listrik",
+            English: "Electric"
+          },
+          BillAmount: "50000.00",
+          BillNumber: "1133557799",
+          BillSubCompany: "00001"
+        },
+        {
+          BillDescription: {
+            Indonesian: "Air",
+            English: "Water"
+          },
+          BillAmount: "30000.00",
+          BillNumber: "2244668800",
+          BillSubCompany: "00001"
+        }
+      ]),
+      FreeTexts: JSON.stringify([
+        {
+          Indonesian: "Free Text 1",
+          English: "Free Text 1"
+        },
+        {
+          Indonesian: "Free Text 2",
+          English: "Free Text 2"
+        },
+      ]),
+      AdditionalData: JSON.stringify({}),
+      PaymentFlagReason: JSON.stringify({}),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },], {});
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('bca_transactions', null, {});
--- seeders/20240417063050-payment-data.js
+++ seeders/20240417063050-payment-data.js
@@ -2,8 +2,8 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('bca_payments', [{
-      CompanyCode: "12345",
-      CustomerNumber: "ABC0012300DEF",
+      CompanyCode: "14348",
+      CustomerNumber: "087875568390",
       RequestID: "201507131507262221400000001975",
       PaymentFlagStatus: "00",
       PaymentFlagReason: JSON.stringify({ Indonesian: 'Sukses', English: 'Success' }),
@@ -26,8 +26,80 @@ module.exports = {
       AdditionalData: ""
     },
     {
-      CompanyCode: "12340",
-      CustomerNumber: "ABC0012300DEB",
+      CompanyCode: "14348",
+      CustomerNumber: "081210704479",
+      RequestID: "201507131507262221400000001977",
+      PaymentFlagStatus: "00",
+      PaymentFlagReason: JSON.stringify({ Indonesian: 'Sukses', English: 'Success' }),
+      CustomerName: "Customer BCA Virtual Account",
+      CurrencyCode: "IDR",
+      PaidAmount: "150000.00",
+      TotalAmount: "100000.00",
+      TransactionDate: "15/03/2014 22:07:40",
+      DetailBills: JSON.stringify([]),
+      FreeTexts: JSON.stringify([
+        {
+          Indonesian: "Free Text 1",
+          English: "Free Text 1"
+        },
+        {
+          Indonesian: "Free Text 2",
+          English: "Free Text 2"
+        },
+      ]),
+      AdditionalData: ""
+    },
+    {
+      CompanyCode: "14348",
+      CustomerNumber: "081284618005",
+      RequestID: "201507131507262221400000001977",
+      PaymentFlagStatus: "00",
+      PaymentFlagReason: JSON.stringify({ Indonesian: 'Sukses', English: 'Success' }),
+      CustomerName: "Customer BCA Virtual Account",
+      CurrencyCode: "IDR",
+      PaidAmount: "150000.00",
+      TotalAmount: "100000.00",
+      TransactionDate: "15/03/2014 22:07:40",
+      DetailBills: JSON.stringify([]),
+      FreeTexts: JSON.stringify([
+        {
+          Indonesian: "Free Text 1",
+          English: "Free Text 1"
+        },
+        {
+          Indonesian: "Free Text 2",
+          English: "Free Text 2"
+        },
+      ]),
+      AdditionalData: ""
+    },
+    {
+      CompanyCode: "14348",
+      CustomerNumber: "082387403355",
+      RequestID: "201507131507262221400000001977",
+      PaymentFlagStatus: "00",
+      PaymentFlagReason: JSON.stringify({ Indonesian: 'Sukses', English: 'Success' }),
+      CustomerName: "Customer BCA Virtual Account",
+      CurrencyCode: "IDR",
+      PaidAmount: "150000.00",
+      TotalAmount: "100000.00",
+      TransactionDate: "15/03/2014 22:07:40",
+      DetailBills: JSON.stringify([]),
+      FreeTexts: JSON.stringify([
+        {
+          Indonesian: "Free Text 1",
+          English: "Free Text 1"
+        },
+        {
+          Indonesian: "Free Text 2",
+          English: "Free Text 2"
+        },
+      ]),
+      AdditionalData: ""
+    },
+    {
+      CompanyCode: "14348",
+      CustomerNumber: "089526632439",
       RequestID: "201507131507262221400000001977",
       PaymentFlagStatus: "00",
       PaymentFlagReason: JSON.stringify({ Indonesian: 'Sukses', English: 'Success' }),
